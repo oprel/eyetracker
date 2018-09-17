c96623301f4be1c8cd4903a0679bc957f7c4b4d1
Unknown <jasperoprel@hotmail.com>
2018-09-17 14:34:52 +0200

    [+]  Particle explosion, dissolving objects

---
+++ Assets/Scripts/gazeParticles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+[RequireComponent(typeof(GazeAware),typeof(ParticleSystem))]
+
+public class gazeParticles : MonoBehaviour {
+
+    private GazeAware aware;
+    private ParticleSystem.EmissionModule ps;
+	private float baserate;
+
+	void Awake () {
+        aware = GetComponent<GazeAware>();
+		ps = GetComponent<ParticleSystem>().emission;
+		ps.rateOverTimeMultiplier=0;
+    }
+	
+	void Update () {
+		if(aware.HasGazeFocus) {
+			ps.rateOverTimeMultiplier = Mathf.Lerp(ps.rateOverTimeMultiplier,1,.1f);
+        }
+        else {
+            ps.rateOverTimeMultiplier = Mathf.Lerp(ps.rateOverTimeMultiplier,0,.3f);
+        }
+	}
+}
---
+++ Assets/updateLookPosition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class updateLookPosition : MonoBehaviour {
+
+	public Material[] dissolveMaterials;
+	private Vector2 lastFocus;
+	private Camera cam;
+	// Use this for initialization
+	
+	private void Awake () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 focus = TobiiAPI.GetGazePoint().Screen;
+		if (focus!=lastFocus){
+			lastFocus = focus;
+			Ray ray = cam.ScreenPointToRay(focus);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity)){
+				Debug.DrawRay(ray.origin, ray.direction * 1000, Color.yellow);
+				foreach(Material m in dissolveMaterials){
+					m.SetVector("_Center",hit.point);
+				}
+			}
+            
+		}
+	}
+}
